eafa83b835e5152bbefaa5f40ee6b4a07a910231
Filip Valentíny <valentinyfilip@protonmail.com>
2024-12-01 09:30:49 +0100

    day 1 both parts

---
+++ AdventOfCode/Days/Day01.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode.Extensions;
+
+namespace AdventOfCode.Days;
+
+public class Day01 : BaseDay
+{
+    private readonly string _input;
+
+    public Day01()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var numbers = _input.GetLines()
+            .Select(line =>
+                line.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries))
+            .Select(parts => new { Col1 = parts[0].StringToInt(), Col2 = parts[1].StringToInt() })
+            .ToList();
+
+        var list1 = numbers.Select(x => x.Col1).OrderBy(n => n).ToList();
+        var list2 = numbers.Select(x => x.Col2).OrderBy(n => n).ToList();
+
+        int result = list1.Zip(list2, (n1, n2) => Math.Abs(n1 - n2)).Sum();
+
+        return ValueTask.FromResult(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var numbers = _input.GetLines()
+            .Select(line =>
+                line.Split([' ', '\t'], StringSplitOptions.RemoveEmptyEntries))
+            .Select(parts => new { Col1 = parts[0].StringToInt(), Col2 = parts[1].StringToInt() })
+            .ToList();
+
+        var keys = numbers.Select(x => x.Col1).OrderBy(n => n).ToList();
+        var values = numbers.Select(x => x.Col2).OrderBy(n => n).ToList();
+
+        int result = keys.Select(x => x * values.Count(v => v == x)).Sum();
+
+        return ValueTask.FromResult(result.ToString());
+    }
+}
