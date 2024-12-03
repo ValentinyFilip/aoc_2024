4482bdbf33fcbc5c65cb51da0dde0ced231b834e
Filip Valentíny <valentinyfilip@protonmail.com>
2024-12-03 18:12:12 +0100

    day 3 done

--- AdventOfCode/Days/Day03.cs
+++ AdventOfCode/Days/Day03.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.Days;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Days;
 
 public sealed class Day03 : BaseDay
 {
@@ -9,7 +11,42 @@ public sealed class Day03 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => throw new NotImplementedException();
+    public override ValueTask<string> Solve_1()
+    {
+        var numbers = Regex.Matches(_input, @"mul\((\d+),(\d+)\)")
+            .Select(match => (int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value)))
+            .ToList();
 
-    public override ValueTask<string> Solve_2() => throw new NotImplementedException();
+        int result = numbers.Select(number => number.Item1 * number.Item2).Sum();
+
+        return new ValueTask<string>(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        bool shouldMul = true;
+
+        int result = Regex.Matches(_input, @"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
+            .Select(match =>
+            {
+                if (match.Value.StartsWith("mul"))
+                {
+                    if (shouldMul)
+                    {
+                        return match.Groups[1].Value.StringToInt() * match.Groups[2].Value.StringToInt();
+                    }
+                }
+                else
+                    shouldMul = match.Value switch
+                    {
+                        "do()" => true,
+                        "don't()" => false,
+                        _ => shouldMul
+                    };
+
+                return 0;
+            }).Sum();
+
+        return new ValueTask<string>(result.ToString());
+    }
 }
